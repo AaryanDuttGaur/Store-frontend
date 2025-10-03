b68b6400942dbc6697f8517b679baffa95180bb4
Aryan dutt Gaur <arushsharma2984@gmail.com>
2025-10-03 14:35:51 +0530

    Fix Login url and signup url

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -36,7 +36,7 @@ export default function SignupPage() {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/auth/signup/", {
+   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/signup/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
